7a873d26c2e12889291e5d995a19f901bbae5900
Nipa2345 <patelnipaj74@gmail.com>
2022-03-04 20:39:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+/*In this example we can reduce the code or
+and simplify the form code using some technique in react*/
 
-function App() {
+import React, { useState } from 'react';
+import './index.css';
+
+const App = () => {
+
+  //Hook: useState.
+  //we can pass object inside useStat()
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    phone: ""
+  });
+
+  //Fat arrow function for all input fields
+  const inputEvent = (event) => {
+    //console.log(event.target.value);
+    //console.log(event.target.name);
+
+    /* We can make this two line in one using destructuring */
+    //const value = event.target.value;
+    //const name = event.target.name;
+    const { value,name } = event.target;
+
+    //Callback function using fat arrow fun.
+    setFullName((preValue) => {
+      //console.log(preValue);
+
+      if (name === "fName") {
+        return {
+          fname: value,
+          lname: preValue.lname,
+          email: preValue.email,
+          phone: preValue.phone,
+        };
+      } else if (name === "lName") {
+        return {
+          fname: preValue.fname,
+          lname: value,
+          email: preValue.email,
+          phone: preValue.phone,
+        };
+      }
+      else if (name === "email") {
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: value,
+          phone: preValue.phone,
+        };
+      }
+      else if (name === "phone") {
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: preValue.email,
+          phone: value,
+        };
+      }
+    });
+  };
+
+  //submit button event
+  const onSubmits = (event) => {
+    event.preventDefault();
+    alert("Form Submitted");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div>
+        <form onSubmit={onSubmits} >
+          <div>
+            <h1> Hello {fullName.fname} {fullName.lname} </h1>
+            <p> {fullName.email} </p>
+            <p> {fullName.phone} </p>
 
+            {/* it's called a controlled components */}
+            <input 
+              type="text"
+              placeholder="Enter Your First Name"
+              name="fName"
+              onChange={inputEvent} //user type anything then this onChange event will call.
+              value={fullName.fname} //Single source of truth. // user type anything ....
+            />
+            <input 
+              type="text"
+              placeholder="Enter Your Last Name"
+              name="lName"
+              onChange={inputEvent}
+              value={fullName.lname}
+            />
+            <input 
+              type="email"
+              placeholder="Enter Your Email Id"
+              name="email"
+              onChange={inputEvent}
+              value={fullName.email}
+              autoComplete = "off"
+            />
+            <input 
+              type="number"
+              placeholder="Enter Your Phone Number"
+              name="phone"
+              onChange={inputEvent}
+              value={fullName.phone}
+            />
+            <button type="Submit"> Submit </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 export default App;
